001f4c0683492151f0b91cd5ee34a6a4263c275c
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-06-22 20:23:21 +0200

    composer-debian work now

---
+++ debian/templates.cs
@@ -0,0 +1,15 @@
+Template: composer/UPDATE_APPS
+Type: boolean
+_description: Update applications with new composer ?
+ Find all /usr/share/*/composer.json + /usr/lib/*/composer.json and perform
+ updates ?
+Default: false
+Description-cs: 
+
+Template: composer/WEB_USER
+Type: string
+_description: Chown system-wide composer files
+ Chown all in  /var/lib/composer/  to given user ?
+Default: www-data
+Default-cs: 
+Description-cs: 
