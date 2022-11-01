0212f079785aa4e7c4bc3e988bfe9f3cf0f94786
Vítězslav Dvořák <info@vitexsoftware.cz>
2022-11-01 14:08:50 +0100

    v1.5 release

---
+++ debian/mkico.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+PACKAGE="composer-debian"
+SRC="$PACKAGE.svg"
+DIR="debian/$PACKAGE/usr/share/icons/hicolor"
+
+resolutions='16 32 48 64 128 256 512 1024'
+
+for resolution in $resolutions
+do
+    mkdir -p $DIR/${resolution}x${resolution}/apps/
+    inkscape -w ${resolution} -h ${resolution} $SRC --export-filename=$DIR/${resolution}x${resolution}/apps/$PACKAGE.png
+done
+mkdir -p $DIR/scalable/apps
+cp $SRC $DIR/scalable/apps/$PACKAGE.svg 
+echo All done
