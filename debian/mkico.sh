3d46ff793038c793e3284a2b348ce80865de29c6
CyberVitexus <info@vitexsoftware.cz>
2025-01-26 15:48:33 +0100

    code update II

--- debian/mkico.sh
+++ debian/mkico.sh
@@ -8,7 +8,7 @@ resolutions='16 32 48 64 128 256 512 1024'
 for resolution in $resolutions
 do
     mkdir -p $DIR/${resolution}x${resolution}/apps/
-    inkscape -w ${resolution} -h ${resolution} $SRC --export-filename=$DIR/${resolution}x${resolution}/apps/$PACKAGE.png
+    inkscape -w "${resolution}" -h "${resolution}" $SRC --export-filename=$DIR/${resolution}x${resolution}/apps/$PACKAGE.png
 done
 mkdir -p $DIR/scalable/apps
 cp $SRC $DIR/scalable/apps/$PACKAGE.svg 
